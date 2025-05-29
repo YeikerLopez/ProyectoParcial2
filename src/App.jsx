f280a6341eccf203970253523b4e5cd8f9ea631b
Yeiker Lopez <lopezyeiker593@gmail.com>
2025-05-28 21:39:10 -0500

    Eliminando importaciones vacias

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import HomePage from './components/HomePage/HomePage';
